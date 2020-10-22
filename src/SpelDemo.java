1d99f5b7fac59a3cc963098d2ad6e3fd088f5fe6
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-10-22 09:25:54 +0200

    test av branch merge

--- src/SpelDemo.java
+++ src/SpelDemo.java
@@ -9,9 +9,9 @@ public class SpelDemo {
 
     SpelDemo(){
 
-        System.out.println("Test av peter");
+        System.out.println("Test av peter, test");
 
-        System.out.println("Test av Ulf");
+        System.out.println("Test av Ulf, test");
 
     }
 
