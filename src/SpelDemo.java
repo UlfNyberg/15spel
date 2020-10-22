0155b2e1d2d8819f3f26781f1d8fa1080182f2a3
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-10-22 09:09:53 +0200

    Initial commit

---
+++ src/SpelDemo.java
@@ -0,0 +1,22 @@
+/**
+ * Created by: Ulf Nyberg
+ * Date: 2020-10-22
+ * Time: 09:09
+ * Project: 15spel
+ * Copyright: MIT
+ */
+public class SpelDemo {
+
+    SpelDemo(){
+
+
+
+        
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
