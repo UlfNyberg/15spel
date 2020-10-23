bc3c1fd09b7601f8a2bd6fa3aea60ff7826a0551
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-10-23 14:52:50 +0200

    Labels and counter logic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -3,7 +3,9 @@ import java.util.Random;
 
 public class GameLogic {
 
-    public static JButton[][] createButtonDisrder(JButton [][] buttonArray) {
+    private static int moveCounter = 0;
+
+    public static JButton[][] createButtonDisrder(JButton[][] buttonArray) {
         Random random = new Random();
 
         for (int i = buttonArray.length - 1; i > 0; i--) {
@@ -20,15 +22,16 @@ public class GameLogic {
         return buttonArray;
     }
 
-    public static JButton[][] swapWithBlackTile(JButton[][] buttonArray, JButton selectedTile){
+    public static JButton[][] swapWithBlackTile(JButton[][] buttonArray, JButton selectedTile) {
 
         int blackTileX = -1;
         int blackTileY = -1;
         JButton blackTile = null;
+        boolean hasMoved = false;
 
-        for (int i = 0; i < buttonArray.length ; i++) {
+        for (int i = 0; i < buttonArray.length; i++) {
             for (int j = 0; j < buttonArray[i].length; j++) {
-                if(buttonArray[i][j].getText().equals("")){
+                if (buttonArray[i][j].getText().equals("")) {
                     blackTile = buttonArray[i][j];
                     blackTileX = j;
                     blackTileY = i;
@@ -39,39 +42,46 @@ public class GameLogic {
         System.out.println("Black tile x= " + blackTileX);
         System.out.println("Black tile y= " + blackTileY);
 
-        if(blackTileY != 0) {
+        if (blackTileY != 0) {
             if (buttonArray[blackTileY - 1][blackTileX] == selectedTile) {
                 System.out.println("selectedTile är ovanför");
                 buttonArray[blackTileY][blackTileX] = selectedTile;
                 buttonArray[blackTileY - 1][blackTileX] = blackTile;
+                hasMoved = true;
             }
         }
-        if(!(blackTileY >= buttonArray.length - 1)) {
+        if (!(blackTileY >= buttonArray.length - 1)) {
             if (buttonArray[blackTileY + 1][blackTileX] == selectedTile) {
                 System.out.println("selectedTile är nedanför");
                 buttonArray[blackTileY][blackTileX] = selectedTile;
                 buttonArray[blackTileY + 1][blackTileX] = blackTile;
+                hasMoved = true;
             }
         }
-        if(blackTileX != 0) {
+        if (blackTileX != 0) {
             if (buttonArray[blackTileY][blackTileX - 1] == selectedTile) {
                 System.out.println("selectedTile är till vänster");
                 buttonArray[blackTileY][blackTileX] = selectedTile;
                 buttonArray[blackTileY][blackTileX - 1] = blackTile;
+                hasMoved = true;
             }
         }
-        if(!(blackTileX >= buttonArray[0].length - 1)) {
+        if (!(blackTileX >= buttonArray[0].length - 1)) {
             if (buttonArray[blackTileY][blackTileX + 1] == selectedTile) {
                 System.out.println("selectedTile är till höger");
                 buttonArray[blackTileY][blackTileX] = selectedTile;
                 buttonArray[blackTileY][blackTileX + 1] = blackTile;
+                hasMoved = true;
             }
         }
+
+        if (hasMoved)
+            moveCounter++;
 
         return buttonArray;
     }
 
-    public static boolean checkVictoryCondition(JButton[][] buttonArray){
+    public static boolean checkVictoryCondition(JButton[][] buttonArray) {
         boolean solved;
 
         int counter = 1;
@@ -80,35 +90,29 @@ public class GameLogic {
             for (int col = 0; col < buttonArray[row].length; col++) {
                 if ((buttonArray[row][col].getText().equals(Integer.toString(counter)))) {
                     System.out.println("ButtonGetText " + buttonArray[row][col].getText());
-                    if (counter == (buttonArray.length*buttonArray[0].length-1)) {
+                    if (counter == (buttonArray.length * buttonArray[0].length - 1)) {
                         System.out.println("Grattis! Du har klarat spelet!");
                         return (true);
                     }
 
-                }
-                else
+                } else
                     break;
                 counter++;
-                //else
-                    //return false;
+
             }
         }
 
         System.out.println(counter + " Countern");
 
-        /*
-        if (counter == (buttonArray.length*buttonArray[0].length-1)) {
-            System.out.println("Grattis! Du har klarat spelet!");
-            return (true);
-        }
-
-         */
-
-                return (false);
-
-
-        }
+        return (false);
 
 
     }
+
+    public static int getMoveCounter() {
+        return moveCounter;
+    }
+
+
+}
 
